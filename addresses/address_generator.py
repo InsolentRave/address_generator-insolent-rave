d20db45851719e7081139e968992a2ae62c1e2b7
acheca <acheca@humboldt.org.co>
2022-09-05 00:11:49 -0500

    comment on functions

--- addresses/address_generator.py
+++ addresses/address_generator.py
@@ -4,6 +4,8 @@ from hdwallet.symbols import BTC, ETH, LTC, TRX
 
 from .errors import AddressPathError, NotSupportedCurrency, PrivateKeyError
 
+
+# Factories for each symbol to create basic wallet object:
 symbol_to_wallet_factory = {
     'BTC': lambda: BIP32HDWallet(symbol=BTC),
     'ETH': lambda: BIP44HDWallet(symbol=ETH),
@@ -13,23 +15,32 @@ symbol_to_wallet_factory = {
 
 
 def generate_address(symbol, path):
+    """
+    Generates the address from a currency symbol and a path.
+    """
     try:
         wallet = symbol_to_wallet_factory[symbol]()
     except KeyError:
         raise NotSupportedCurrency(symbol)
 
-    set_path(wallet, path)
+    set_private_key(wallet, path)
+    # Generate the address from the child private key
     return wallet.address()
 
 
-def set_path(wallet, path):
+def set_private_key(wallet, path):
+    """
+    Sets the child private key of the wallet based on the key path.
+    """
     try:
+        # Sets the master private key of the wallet:
         wallet.from_xprivate_key(xprivate_key=settings.ZEPLY_XPRIVATE_KEY, strict=True)
 
     except ValueError as exc:
         raise PrivateKeyError(exc)
 
     try:
+        # Selects the child private key for generating the address based on the path:
         wallet.from_path(path)
     except Exception as exc:
         raise AddressPathError(exc)
